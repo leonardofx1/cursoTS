3df8fc45b0eff2ce5f839f33c864b91eac25f34a
leonardo <102767490+leonardofx1@users.noreply.github.com>
2023-10-15 20:22:44 -0300

    narrowing

--- 3_AVANCANDO_EM_TIPOS/index.js
+++ 3_AVANCANDO_EM_TIPOS/index.js
@@ -17,3 +17,55 @@ const pessoa = (user) => {
 };
 const usuario = { nome: 'leonardo', age: 23 };
 pessoa(usuario);
+// props opcionais
+const casa = (quartos, color) => {
+    console.log(quartos, color);
+};
+casa(2, 'red');
+casa(5);
+const carro = (ano, cor) => {
+    if (cor !== undefined) {
+        console.log(cor, ano);
+    }
+    else {
+        console.log(ano);
+    }
+};
+carro(2023);
+carro(2020, 'blue');
+// Union types
+const time = (jogador) => {
+    if (typeof jogador === 'string')
+        return `espero que goste do nosso time ${jogador}`;
+    else {
+        return 'em busca de um jovem talento';
+    }
+};
+console.log(time('leonardo'));
+console.log(time(false));
+const computador = {
+    marca: 'dell',
+    memoria: "hd",
+    memoriaRam: 2
+};
+console.log(computador);
+const club = (jogador) => {
+    if (typeof jogador === 'string')
+        return `espero que goste do nosso time ${jogador}`;
+    else {
+        return 'em busca de um jovem talento';
+    }
+};
+console.log(club('leonardo'));
+console.log(club('felipe'));
+const aluno = {
+    material: "notebook",
+    age: 23,
+    name: 'leonardo',
+    sexo: 'M'
+};
+console.log(aluno);
+//bigint
+let num;
+num = 1000n;
+console.log(num);
--- 3_AVANCANDO_EM_TIPOS/index.ts
+++ 3_AVANCANDO_EM_TIPOS/index.ts
@@ -31,3 +31,94 @@ const pessoa = (user:{nome:string, age:number})=> {
 const usuario = {nome:'leonardo', age:23}
 
 pessoa(usuario)
+
+// props opcionais
+
+const casa = (quartos : number, color ?: string) => {
+    console.log(quartos, color)
+}
+casa(2, 'red')
+casa(5)
+
+const carro = (ano:number, cor?: string) => {
+    if(cor !== undefined) {
+        console.log(cor, ano)
+    }
+    else {
+        console.log(ano)
+    }
+}
+
+carro(2023)
+
+carro(2020, 'blue')
+
+// Union types
+
+const time = (jogador: string | false) : string  => {
+if (typeof jogador === 'string')
+return `espero que goste do nosso time ${jogador}`
+else {
+    return 'em busca de um jovem talento'
+}
+
+}
+
+console.log(time('leonardo'))
+console.log(time(false))
+
+// type alias
+
+type Jogador = string | false
+type Computador ={
+    memoriaRam:number,
+    memoria: 'ssd' | 'hd',
+    marca:string
+}
+
+const computador: Computador = {
+     marca:'dell',
+     memoria:"hd",
+     memoriaRam:2
+} 
+console.log(computador)
+const club = (jogador: Jogador) : string  => {
+if (typeof jogador === 'string')
+return `espero que goste do nosso time ${jogador}`
+else {
+    return 'em busca de um jovem talento'
+}
+
+}
+
+console.log(club('leonardo'))
+console.log(club('felipe'))
+
+// interface 
+
+interface Aluno {
+    material: 'caderno' | 'notebook'
+    age: number
+    name:string
+}
+
+const aluno:Aluno = {
+    material:"notebook",
+    age:23,
+    name:'leonardo',
+    sexo:'M'
+}
+
+console.log(aluno)
+
+interface Aluno {
+    sexo: 'F' | 'M'
+}
+
+//bigint
+
+let num:bigint
+
+num = 1000n
+
+console.log(num)
